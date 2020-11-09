6139bf1476f17e569ea22823b28d14f81ed07e1b
Eric Baer <me@ericbaer.com>
2020-11-09 11:19:08 -0800

    Use starship, remove clutter, fix cnflicting alias'

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -55,12 +55,4 @@ then
 fi
 
 # List all files colorized in long format
-alias ll="ls -alF"
-# Long form no user group, color
-alias l="ls -oG"
-# Order by last modified, long form no user group, color
-alias lt="ls -toG"
-# List all except . and ..., color, mark file types, long form no user group, file size
-alias la="ls -AGFoh"
-# List all except . and ..., color, mark file types, long form no use group, order by last modified, file size
-alias lat="ls -AGFoth"
+alias ll="ls -alF --group-directories-first --sort=extension"
